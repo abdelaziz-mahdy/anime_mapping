c059ee8fdd9f3c5f77f48249e746e1bd491cd085
Abdelaziz Mahdy <zezohassam@gmail.com>
2022-12-24 09:58:21 +0200

    comment code

--- get_latest.py
+++ get_latest.py
@@ -3,11 +3,11 @@ from common import *
 mappingFile="latest.json"
 
 latest=readJsonFile(mappingFile)
-#revert to only last episode
-for key,value in latest.items():
-    if isinstance(value,str):
-        if value.startswith("EP"):
-            latest[key]=int(value[indexOfLastCharacterOfSubstring(value,"EP "):value.index("/")])
+# #revert to only last episode
+# for key,value in latest.items():
+#     if isinstance(value,str):
+#         if value.startswith("EP"):
+#             latest[key]=int(value[indexOfLastCharacterOfSubstring(value,"EP "):value.index("/")])
 page = requests.get("https://api.consumet.org/anime/gogoanime/recent-episodes")
 # print(page.content)
 jsonObject=page.json()
