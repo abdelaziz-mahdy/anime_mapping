a98bf598739e5b12548968f39306a32f65c7ea3f
Abdelaziz Mahdy <zezohassam@gmail.com>
2023-02-13 20:51:48 +0200

    added print

--- get_latest.py
+++ get_latest.py
@@ -13,7 +13,7 @@ gotNew=True
 while gotNew:
     print(f"loading page {page}")
     pageData = requests.get(f"https://api.consumet.org/anime/gogoanime/recent-episodes?page={page}")
-    # print(page.content)
+    print(page.content)
     jsonObject=pageData.json()
     for anime in jsonObject["results"]:         
         if anime["id"] in latest and latest[anime["id"]]==anime["episodeNumber"] :
