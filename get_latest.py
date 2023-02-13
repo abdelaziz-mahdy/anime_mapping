eaf85ad4e6113c43857bcf728618718c491c4cbd
Abdelaziz Mahdy <zezohassam@gmail.com>
2023-02-13 21:20:42 +0200

    add user agent

--- get_latest.py
+++ get_latest.py
@@ -12,7 +12,7 @@ page=1
 gotNew=True
 while gotNew:
     print(f"loading page {page}")
-    pageData = requests.get(f"https://api.consumet.org/anime/gogoanime/recent-episodes?page={page}")
+    pageData = requests.get(f"https://api.consumet.org/anime/gogoanime/recent-episodes?page={page}",headers={"Content-Type": "application/json","User-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"})
     print(pageData.content)
     jsonObject=pageData.json()
     for anime in jsonObject["results"]:         
